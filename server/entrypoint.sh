99120009ef8694dfaa183a9203376763657513bd
yacn <admin@yacn.me>
2015-03-02 11:48:07 -0500

    updates server entrypoint script to run migrations

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-. /hatch_venv/bin/activate && python runserver.py -h 0.0.0.0 -p 5000
+. /hatch_venv/bin/activate
+python manage.py db init
+python manage.py db migrate
+python manage.py db upgrade
+python runserver.py -h 0.0.0.0 -p 5000
