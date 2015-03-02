178060e4bf420767232f9875092afe2081964ab8
yacn <admin@yacn.me>
2015-03-02 16:49:46 -0500

    Updates with new environment variable names

--- server/neuhatch/config.py
+++ server/neuhatch/config.py
@@ -12,18 +12,25 @@ class Config:
         self.access_token = None
         self.access_token_secret = None
         self.app_secret = None
-        self.database_url = None
+        self.database_uri = None
         self.hostname = "http://localhost:5000/"
     def environ_set(self):
-        self.set_with_warning("consumer_key")
-        self.set_with_warning("consumer_secret")
-        self.set_with_warning("access_token")
-        self.set_with_warning("access_token_secret")
-        self.set_with_warning('app_secret')
-        self.set_with_warning("database_url")
+        self.set_with_warning("CONSUMER_KEY")
+        self.set_with_warning("CONSUMER_SECRET")
+        self.set_with_warning("ACCESS_TOKEN")
+        self.set_with_warning("ACCESS_TOKEN_SECRET")
+        self.set_with_warning('APP_SECRET')
+        self.set_with_warning('HATCH_DB_USER')
+        self.set_with_warning('HATCH_DB_PASSWORD')
+        self.set_with_warning('HATCH_DB_NAME')
+        self.database_uri = os.getenv('DATABASE_URI',
+            'postgres://{usr}:{pw}@db/{db}'.format(
+                usr=self.hatch_db_user, pw=self.hatch_db_password,
+                db=self.hatch_db_name))
 
     def set_with_warning(self, var):
         val = os.environ.get(var)
+        var = var.lower() # yay mutation
         setattr(self, var, val)
         if getattr(self, var) is None:
             print "Warning %s is None" % var
